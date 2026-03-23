f29fa0656f8eae8c1e7243ff11a3ff4849d45f82
rodaguil <rodaguil@student.42madrid.com>
2026-03-23 14:44:11 +0100

    previous, target node, cheapest and above median added to the struct

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -2,10 +2,17 @@
 # define PUSH_SWAP_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <limits.h>
 
 typedef struct s_stack_node
 {
+    int                     indexing;
+    int                     push_cost;
     int                     value;
+    int                     abv_median_ln;
+    int                     cheapest;
     struct s_stack_node     *next;
+    struct s_stack_node     *prev;
+    struct s_stack_node     *trgt_node;
 }   t_stack_node;
 #endif
